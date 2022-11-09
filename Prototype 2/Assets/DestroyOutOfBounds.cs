73c6dd208d7b3e1591194d1c71a69c49320437de
HurtadoDiego <hurtado.baker.diego@gmail.com>
2022-11-08 22:03:20 -0600

    Reto y prototipo 2

---
+++ Prototype 2/Assets/DestroyOutOfBounds.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 30;
+
+    public float lowBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound){
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowBound) {
+            Debug.Log("Game over!");
+            Destroy(gameObject);
+        }
+    }
+}
